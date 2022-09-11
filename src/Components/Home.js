7bfda1c380794eabde5f459872d661dc1a12d6bf
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-09-11 13:40:38 +0300

    screens

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -60,10 +60,18 @@ console.log(allProducts);
             ) : (
             <>
             <p> Loading products, please wait. </p >
+            <br/>
               <Stack spacing={1}>
                 <Skeleton variant="text" />
                 <Skeleton variant="circular" width={40} height={40} />
                 <Skeleton variant="rectangular" width={210} height={118} />
+                <Skeleton variant="text" />
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="rectangular" width={210} height={118} />
+                <Skeleton variant="text" />
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="rectangular" width={210} height={118} />
+                
               </Stack>
               </>)
           }
